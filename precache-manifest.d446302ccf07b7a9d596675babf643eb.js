d7260126e079c83e4be354518f3f94b20db8c1a4
Ashley Hopkins <ashhopkins@ashleys-mbp.home>
2019-05-05 21:27:32 +0100

    Updates

---
+++ precache-manifest.d446302ccf07b7a9d596675babf643eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2289a0821bdcc8fa82972aaff7db525",
+    "url": "/ashcode1/email-app/index.html"
+  },
+  {
+    "revision": "8e1acaf305a321e756dd",
+    "url": "/ashcode1/email-app/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ashcode1/email-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "8e1acaf305a321e756dd",
+    "url": "/ashcode1/email-app/static/js/main.3d5755d0.chunk.js"
+  },
+  {
+    "revision": "30b01024a24ffda0a607",
+    "url": "/ashcode1/email-app/static/js/runtime~main.6f2988e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ashcode1/email-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
